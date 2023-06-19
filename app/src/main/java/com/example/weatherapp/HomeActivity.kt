468948a5184aeb6644a6c7893d3d1134154a2ad6
moali2605 <107775491+moali2605@users.noreply.github.com>
2023-06-20 01:06:06 +0300

    bottom navigation bar

--- app/src/main/java/com/example/weatherapp/HomeActivity.kt
+++ app/src/main/java/com/example/weatherapp/HomeActivity.kt
@@ -2,10 +2,25 @@ package com.example.weatherapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.example.weatherapp.databinding.ActivityHomeBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigationrail.NavigationRailMenuView
 
 class HomeActivity : AppCompatActivity() {
+    lateinit var binding: ActivityHomeBinding
+    private lateinit var bottomNavigationBar: BottomNavigationView
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        bottomNavigationBar = binding.bottomNavigationBar
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+        NavigationUI.setupWithNavController(bottomNavigationBar, navController)
     }
 }
